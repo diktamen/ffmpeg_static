b33484b6d8900b72a7e84d118e4f056c745fcbdd
LWAhonen <lauri.ahonen@diktamen.com>
2025-09-10 14:59:24 +0300

    Add ffmpeg to the list

--- build-ffmpeg-dll.bat
+++ build-ffmpeg-dll.bat
@@ -2,8 +2,7 @@
 REM Build FFmpeg with DLL overlay to force shared libraries even with static triplets
 echo Building FFmpeg with DLL overlay for x64, x86, and arm64 architectures...
 
-vcpkg install ffmpeg[avcodec,avdevice,avfilter,avformat,core,swresample,swscale,mp3lame,opus,speex,vorbis]:x64-windows-static-md ffmpeg[avcodec,avdevice,avfilter,avformat,core,swresample,swscale,mp3lame,opus,speex,vorbis]:x86-windows-static-md ffmpeg[avcodec,avdevice,avfilter,avformat,core,swresample,swscale,mp3lame,opus,speex,vorbis]:arm64-windows-static-md --overlay-ports=overlays --classic
+vcpkg install ffmpeg[ffmpeg,avcodec,avdevice,avfilter,avformat,core,swresample,swscale,mp3lame,opus,speex,vorbis]:x64-windows-static-md ffmpeg[ffmpeg,avcodec,avdevice,avfilter,avformat,core,swresample,swscale,mp3lame,opus,speex,vorbis]:x86-windows-static-md ffmpeg[ffmpeg,avcodec,avdevice,avfilter,avformat,core,swresample,swscale,mp3lame,opus,speex,vorbis]:arm64-windows-static-md --overlay-ports=overlays --classic
 
 echo.
-echo Build completed. FFmpeg DLLs should be available in the vcpkg_installed directory.
-pause
+echo Build completed. FFmpeg DLLs should be available in the installed directory.
